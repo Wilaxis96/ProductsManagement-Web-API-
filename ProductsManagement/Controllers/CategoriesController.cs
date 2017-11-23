38a24fc56d96e65c7cdea67b9abd729fa7d7eb89
Wilson Arboleda <wilaxis96@gmail.com>
2017-11-23 14:12:56 -0500

    Initial Commit
    Initial Commit

---
+++ ProductsManagement/Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using ProductsManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ProductsManagement.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CategoriesController : ApiController
+    {
+        private ProductsManagementEntities db = new ProductsManagementEntities();
+
+        // GET: api/Categories
+        public IQueryable<Category> GetCategories()
+        {
+            return db.Categories;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
